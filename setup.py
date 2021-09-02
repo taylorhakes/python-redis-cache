0df737c315f60aae5473769e27910ae62f7f20da
Taylor Hakes <taylorhakes@users.noreply.github.com>
2021-09-01 23:06:09 -0700

    bugfix: add release.yml

--- setup.py
+++ setup.py
@@ -6,8 +6,10 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+__version__ = "1.1.1"
+
 setup(name='python-redis-cache',
-      version='1.1.1',
+      version=__version__,
       description='Basic Redis caching for functions',
       long_description=long_description,
       long_description_content_type='text/markdown',
